eff97076232e7a6d6b0f1fff6eb0c041dafb6bca
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-01-09 10:39:20 -0600

    Add configuration checks to basic example

--- examples/basic.py
+++ examples/basic.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 import pathlib
+import sys
 
 import humphrey
 
 
 def main() -> None:
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     config_file = pathlib.Path(__file__).resolve().with_name("basic.json")
     irc = humphrey.IRCClient(config_file)
     irc.c.pretty = True
     irc.debug = True
+
+    if "irc:channel" not in irc.c:
+        irc.c["irc:channel"] = "#humphrey"
+        logging.warning(f'Edit {irc.c.path} and set "irc:channel"')
+        return
 
     @irc.ee.on("376")
     def on_rpl_endofmotd(_: str, bot: humphrey.IRCClient) -> None:
@@ -18,7 +26,11 @@ def main() -> None:
 
     loop = asyncio.get_event_loop()
     host = irc.c.get("irc:host")
+    if host is None:
+        host = irc.c["irc:host"] = "irc.synirc.net"
     port = irc.c.get("irc:port")
+    if port is None:
+        port = irc.c["irc:port"] = 6660
     coro = loop.create_connection(irc, host, port)
     loop.run_until_complete(coro)
     loop.run_forever()
